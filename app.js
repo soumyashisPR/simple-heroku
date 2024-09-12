ba651c4e9b0c46d7786050b1e826040dc2dd6a59
Soumyashis <soumyashis.pradhan@phonepe.com>
2024-09-13 03:58:30 +0530

    test

--- app.js
+++ app.js
@@ -1,38 +1,51 @@
 const fs = require("fs");
+const http = require("http");
+const WebSocket = require("ws");
 
-// let ngnix know we want to start serving from the proxy
+// let nginx know we want to start serving from the proxy
 fs.openSync("/tmp/app-initialized", "w");
 
-const server = require("http").createServer(function (req, res) {
+// Create an HTTP server
+const server = http.createServer(function (req, res) {
   res.writeHead(200);
   res.end("hello world");
 });
 
-// socket.io setup
-const io = require("socket.io").listen(server);
-io.set("transports", ["websocket"]);
+// Set up WebSocket server
+const wss = new WebSocket.Server({ server });
 
-// listen to ngnix socket
+// Handle WebSocket connection
+wss.on("connection", function (ws) {
+  console.info("Client connected");
+
+  // Handle WebSocket messages
+  ws.on("message", function (message) {
+    console.info("Received message: ", message);
+
+    // Assuming we handle a 'login' message as an example
+    try {
+      const parsedMessage = JSON.parse(message);
+      if (parsedMessage.type === "login") {
+        // Send back a response to the client
+        ws.send(JSON.stringify({ message: parsedMessage.data }));
+      }
+    } catch (err) {
+      console.error("Error parsing message:", err);
+    }
+  });
+
+  // Handle WebSocket disconnection
+  ws.on("close", function () {
+    console.info("Client disconnected");
+  });
+
+  // Handle WebSocket errors
+  ws.on("error", function (err) {
+    console.error("WebSocket error:", err);
+  });
+});
+
+// Listen on the Unix socket for NGINX
 server.listen("/tmp/nginx.socket", function () {
-  console.info(`server up`);
-});
-
-io.on("connect_error", function (err) {
-  console.error(err);
-});
-
-io.on("connect_timeout", function (err) {
-  console.error(err);
-});
-
-io.on("connection", function (socket) {
-  console.info(`socket: ${socket.id} connected`);
-
-  socket.on("disconnect", function () {
-    console.info("disconnected");
-  });
-
-  socket.on("login", (message) => {
-    socket.emit("login", { message: message });
-  });
+  console.info("Server is up");
 });
